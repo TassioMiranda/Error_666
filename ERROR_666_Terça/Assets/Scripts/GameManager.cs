90482af0a265b770b591aab53a39eaa2adc1f1fe
Tassio <tassiomiranda77@gmail.com>
2017-12-12 03:17:08 -0300

    ERROR_666
    Steven, aqui está, você já é um contribuidor

---
+++ ERROR_666_Terça/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager _Instance;
+
+	void Awake () {
+		#region
+		DontDestroyOnLoad (_Instance);
+
+		if (_Instance != null) {
+			Destroy (_Instance);
+		}
+
+		if (_Instance == this) {
+		}
+		#endregion
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
