b508614ca49d62b6bcc579df727a6682cc50c93a
Cholin1123 <steven.martinez16@hotmail.com>
2017-12-13 14:57:35 -0300

    TUDO IRMÃO

---
+++ ERROR_666_Terça/Assets/Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager _Instance;
+	public enum _gameStates{Menu, Playing, Reset, Pause};
+	[SerializeField] AudioSource s_backgroundMusic;
+	[SerializeField] AudioClip s_deathSound;
+	public _gameStates gameState = _gameStates.Menu;
+	private bool _pausegame;
+
+	void Awake ()
+	{
+		#region
+		DontDestroyOnLoad (_Instance);
+
+		if (_Instance == null) 
+		{
+			_Instance = this;
+		}
+
+		if (_Instance == this)
+		{
+			Destroy (_Instance);
+		}
+		#endregion
+
+		_pausegame = false;
+	}
+
+	void Update () 
+	{
+		switch(_gameStates)
+		{
+		case _gameStates.Menu:
+			break;
+		case _gameStates.Playing:
+			break;
+		case _gameStates.Pause:
+			break;
+		case _gameStates.Reset:
+			break;
+		}
+	}
+}
---
+++ ERROR_666_Terça/Assets/Assets/Scripts/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	private float posicaoX;
+	private float posicaoY;
+
+	[SerializeField] Gameobject _tesouraPrefab;
+	private GameManager _gameManager;
+	[SerializeField] float _Frequencia;
+	private float frequencia;
+
+
+	void Start () 
+	{
+		_gameManager = FindObjectOfType (typeof(GameManager)) as GameManager;
+
+		_Frequencia = 
+	}
+
+	void Update () 
+	{
+		
+	}
+
+	public void Spawn()
+	{
+			
+	}
+}
